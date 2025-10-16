215cc0bcba7bfdf2f6a5e6f59a93ab4873aedc08
Michael Njuguna <michaelnjuguna184+github@gmail.com>
2025-10-16 11:17:42 +0300

    WIP:(Upgrade) departments table

--- app/Filament/Resources/Departments/DepartmentResource.php
+++ app/Filament/Resources/Departments/DepartmentResource.php
@@ -2,11 +2,9 @@
 
 namespace App\Filament\Resources\Departments;
 
+use App\Filament\Resources\Department\Schemas\DepartmentTable;
 use Filament\Schemas\Schema;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Textarea;
-use Filament\Forms\Components\Select;
-use App\Models\Employee;
+
 use Filament\Tables\Columns\TextColumn;
 use Filament\Actions\ActionGroup;
 use Filament\Actions\EditAction;
@@ -15,15 +13,11 @@ use Filament\Actions\DeleteAction;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use App\Filament\Resources\Departments\Pages\ListDepartments;
-use App\Filament\Resources\DepartmentResource\Pages;
-use App\Filament\Resources\DepartmentResource\RelationManagers;
 use App\Models\Department;
 use Filament\Forms;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 use App\Filament\Resources\Departments\Schemas\DepartmentForm;
 
 class DepartmentResource extends Resource
@@ -40,47 +34,7 @@ class DepartmentResource extends Resource
 
     public static function table(Table $table): Table
     {
-        return $table
-            ->query(
-                static::getEloquentQuery()->with('manager')->latest()
-
-            )
-            ->columns([
-                //
-                TextColumn::make('name')
-                    ->searchable()
-                    ->sortable(),
-                TextColumn::make('code')
-                    ->searchable()
-                    ->sortable()
-                    ->limit(10)
-                    ->label('Department Code'),
-                TextColumn::make('description')
-                    ->limit(50)
-                    ->label('Description'),
-                TextColumn::make('manager_id')
-                    ->formatStateUsing(fn($record) => $record->manager?->full_name ?? 'No Manager')
-                    ->label('Manager')
-                    ->searchable()
-                    ->sortable()
-                    ->toggleable(),
-            ])
-            ->filters([
-                //
-            ])
-            ->recordActions([
-                ActionGroup::make([
-
-                    EditAction::make(),
-                    ViewAction::make(),
-                    DeleteAction::make(),
-                ])
-            ])
-            ->toolbarActions([
-                BulkActionGroup::make([
-                    DeleteBulkAction::make(),
-                ]),
-            ]);
+        return DepartmentTable::configure($table);
     }
 
     public static function getRelations(): array
